e695a49450eb1173fc5cfac72713f8ad7ce141bf
Joshua Leung <aligorith@gmail.com>
2019-03-08 19:45:11 +1300

    Optimised + improved version of prepare_for_web.py
    * Cache profile loading (for faster processing, without needing to reload/recreate
      the profiles for each image processed). This reuslted in a significant improvement
      or around 20-40% reduction in overall processing times in 500-800 photo folders
    
    * Try to look for the AdobeRGB.icc file in the directory where the script lives,
      and only failing that, use the hardcoded path
    
    * Improve output to note progress (including how many files in total are being processed),
      reporting this while processing so that we have a better way of monitoring progress
    
    * Don't print all the names of files found initially. This just doubles up the noise
      in the console. Instead, now, we only report the first 30 or so.

--- prepare_for_web.py
+++ prepare_for_web.py
@@ -4,6 +4,14 @@ import sys
 import os
 import tempfile
 
+try:
+	# Py 3 only
+	import pathlib
+	ICC_DATA_DIR = pathlib.Path(__file__).parents[0]
+except ImportError:
+	# Py 2 - Hardcoded to work with the standard install location
+	ICC_DATA_DIR = r"C:\Users\Joshua\cmdutils"
+
 from PIL import Image
 from PIL import ImageCms
 #from PIL import ExifTags
@@ -19,15 +27,41 @@ def is_adobe_rgb(img):
 	makernote = exif[MAKERNOTE_TAG]
 
 	return makernote[CANON_CS_TAG] == "Adobe RGB"  # XXX: This still doesn't work!
- 
 
-# Colorspace conversion magic 
+
+# Colorspace conversion magic
 def adobe_to_srgb(img):
 	srgb = ImageCms.createProfile('sRGB')
 	
-	icc = open(r'C:\Users\Joshua\cmdutils\AdobeRGB.icc', 'rb')
 	#icc = open('AdobeRGB.icc')
-	 	
+	#icc = open(r'C:\Users\Joshua\cmdutils\AdobeRGB.icc', 'rb')
+	icc = open(os.path.join(ICC_DATA_DIR, 'AdobeRGB.icc'), 'rb')
+	
+	img = ImageCms.profileToProfile(img, icc, srgb)
+	#img = ImageCms.profileToProfile(img, icc, srgb, renderingIntent = ImageCms.INTENT_SATURATION)
+	
+	return img
+
+
+# Fetch profiles to use into a global cache
+COLOR_PROFILES = {}
+
+def init_color_profiles():
+	global COLOR_PROFILES
+	
+	COLOR_PROFILES['srgb'] = ImageCms.createProfile('sRGB')
+	
+	#COLOR_PROFILES['argb'] = ImageCms.getOpenProfile(r'AdobeRGB.icc')
+	COLOR_PROFILES['argb'] = ImageCms.getOpenProfile(os.path.join(ICC_DATA_DIR, 'AdobeRGB.icc'))
+init_color_profiles()
+
+# Colorspace conversion magic (using cached profiles)
+def adobe_to_srgb__fast(img):
+	global COLOR_PROFILES
+	
+	srgb = COLOR_PROFILES['srgb']
+	icc = COLOR_PROFILES['argb']
+	
 	img = ImageCms.profileToProfile(img, icc, srgb)
 	#img = ImageCms.profileToProfile(img, icc, srgb, renderingIntent = ImageCms.INTENT_SATURATION)
 	
@@ -47,7 +81,7 @@ if len(sys.argv) > 1:
 	# Check on each path supplied...
 	print("Checking supplied paths...")
 	images = []
-
+	
 	for path in sys.argv[1:]:
 		if os.path.isdir(path):
 			# Directory - Find everything interesting there to convert
@@ -70,21 +104,40 @@ if len(sys.argv) > 1:
 			images.append(path)
 else:
 	# Hunt for picasa exports / camera raw exports
+	print("sys.argv = %s\n\n" % (sys.argv))
+	
 	print("Finding images...")
 	images = find_images('.')
-	
-print("\n\nFound %d Images:\n   %s\n\n" % (len(images), images))
-if len(images) == 0:  sys.exit(1)
 
-for fileN in images:
-	print("Converting ==> '%s'" % (fileN))
+# Report how many were found
+N = len(images)
+print("\n\nFound %d Images" % (N))
+PREVIEW_LIMIT = 30
+
+if N == 0:
+	# Silently exit if nothing found
+	sys.exit(1)
+elif N < PREVIEW_LIMIT:
+	# Show what we found if there aren't too many
+	# (Use this when debugging, to ensure we're culling the right ones)
+	print("   %s\n\n" % (images))
+else:
+	# Don't print all of them (as then we miss the full count)
+	# (Useful when doing production exports on datasets with 100-600 shots)
+	print("   %s\n    + '[... %d more...]\n\n'" % (images[:50], N - 50))
+
+# Process images
+# TODO: Parallelise this to use more cores?
+for i, fileN in enumerate(images):
+	print("[%3d/%3d] Converting ==> '%s'" % (i, N, fileN))
 	
 	# Open the file for conversion
 	img = Image.open(fileN)
 	exif = img.info['exif']
 
 	#if is_adobe_rgb(img):
-	img =  adobe_to_srgb(img)
+	#img =  adobe_to_srgb(img)
+	img =  adobe_to_srgb__fast(img)
 	
 	# Save file again, replacing the underscore for a "I"
 	# FIXME: This breaks if we use a filename with path embedded
